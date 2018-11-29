94d53a7221ea4456cd1806a38b2b77174e5a55a2
unknown <164005206@qq.com>
2018-11-29 22:12:49 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log(33)
+// css不是js模块，所以需要loader转化
+require('./index.css')
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const ClenWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:path.join(__dirname,'dist'),
+        // name是entry名字，默认main，hash根据打包后文件内容生成的
+        filename:'[name].[hash:8].js'//打包后文件名字
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                // css-loader用来解析处理css文件中的url路径 把css文件变成模块
+                // style-loader 把css文件变成style变迁插入
+                // yarn add css-loader style-loader -D
+                // 多个loader有顺序要求 转换时候是从右往左转换的 
+                loader:['style-loader','css-loader']
+            }
+        ]
+    },
+    
+    plugins:[
+        // 用来自动向模块内部注入变量
+        new webpack.ProvidePlugin({
+            $:'jquery'
+        }),  
+        // yarn add clean-webpack-plugin
+        new ClenWebpackPlugin([path.join(__dirname,'dist')]),
+        // yarn add html-webpack-plugin -D
+        // 此插件自动产出html
+        new HtmlWebpackPlugin({
+            template:'./src/index.html',
+            filename:'index.html',//产生文件名称
+            title:'hah ',
+            hash:true,//会在引入的js里加入查询字符串避免缓存
+            minify:{
+                removeAttributeQuotes:true//属性去掉引号
+            }
+        })
+    ],
+    // yarn add webpack-dev-server -D
+    devServer:{
+        contentBase:'./dist',
+        host:'localhost',
+        port:8080,
+        compress:true,//服务器返回给浏览器的时候是否开启gzip压缩
+    },
+}
